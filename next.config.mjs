5da038b9842ee899d48f50082c8fe7c2bab5a23f
naiwansook <your@email.com>
2026-03-30 13:50:12 +0700

    fix next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: '**.fbcdn.net' },
-      { protocol: 'https', hostname: '**.facebook.com' },
-    ],
-  },
-}
+const nextConfig = {}
 
 export default nextConfig
 ```
-
-กด Save
-
----
-
-## สร้าง tsconfig.json
-```
-notepad tsconfig.json
